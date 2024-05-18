2ce407e020a6be6ce5994196ad266a65b4d89283
natansales221 <natansales221@gmail.com>
2024-05-18 17:04:15 -0300

    trazendo variaveis pra dentro da classe

--- ConsultaDadosAPI.py
+++ ConsultaDadosAPI.py
@@ -6,73 +6,73 @@ import pandas as pd
 from time import sleep
 
 
-parquet_id_dir = r'diretorio'
-headers = {'Content-type': 'application/json', "Authorization":""}
-headers['Authorization'] = 'Bearer ' 
-caminho = r'diretorio'
 
 
 class consulta_cpf():
+    @property
+    def dados(self):
+        return {
+            'parquet_id_dir': r'diretorio',
+            'headers': {'Content-type': 'application/json', "Authorization":"Bearer"},
+            'caminho': r'diretorio'
+            }
+        
 
     def leitura(self):
 
-        for a in os.listdir(caminho):
-           df_read = pd.read_excel(caminho + '\\' + a, engine='openpyxl')
+        for a in os.listdir(self.dados['caminho']):
+           df_read = pd.read_excel(self.dados['caminho'] + '\\' + a, engine='openpyxl')
         
-        r = requests.post("diretorio", headers=headers, data=json.dumps("credenciais"))
-        headers['Authorization'] = 'Bearer ' + r.json()['access_token']
+        request = requests.post("diretorio", headers=self.dados['headers'], data=json.dumps("credenciais"))
+        self.dados['headers']['Authorization'] = 'Bearer ' + request.json()['access_token']
         
-        for c in range(0,2): 
-            for b, id in enumerate(df_read['RESULTADO']):
-                if pd.isna(df_read['status'].iloc[b]):
-                    if df_read['status'].where(df_read['RESULTADO'] == id)[b] != 'ok':     
-                        url = "status"
-                        chamada = requests.get(url + id + 'paginacao_querystring', headers=headers)
+        for b, id in enumerate(df_read['RESULTADO']):
+            if pd.isna(df_read['status'].iloc[b]):
+                if df_read['status'].where(df_read['RESULTADO'] == id)[b] != 'ok':     
+                    url = "status"
+                    chamada = requests.get(url + id + 'paginacao_querystring', headers=self.dados['headers'])
+                    chamada = chamada.json()
+                    if 'message' in chamada:
+                        request = requests.post("diretorio", headers=self.dados['headers'], data=json.dumps("credenciais"))
+                        self.dados['headers']['Authorization'] = 'Bearer ' + request.json()['access_token']
+                        chamada = requests.get(url + id + 'paginacao_querystring', headers=self.dados['headers'])
                         chamada = chamada.json()
-                        if 'message' in chamada:
-                            r = requests.post("diretorio", headers=headers, data=json.dumps("credenciais"))
-                            headers['Authorization'] = 'Bearer ' + r.json()['access_token']
-                            chamada = requests.get(url + id + 'paginacao_querystring', headers=headers)
-                            chamada = chamada.json()
-                            pass
-                        if 'data' in chamada:
-                            df = pd.DataFrame(chamada['data'][0]['resultados'])
-                            # df.to_excel(caminho + f"\\teste{b}.xlsx", index=False)
-                            df = []
-                            df_read.loc[df_read['RESULTADO'] == id, 'status'] = 'ok'
-                           
-                            if (b+1) % 10 == 0:
-                                sleep(60)
-                    else:
                         pass
-        df_read.to_excel(caminho + f"\\teste{b}.xlsx", index=False)
+                    if 'data' in chamada:
+                        df = pd.DataFrame(chamada['data'][0]['resultados'])
+                        # df.to_excel(caminho + f"\\teste{b}.xlsx", index=False)
+                        df = []
+                        df_read.loc[df_read['RESULTADO'] == id, 'status'] = 'ok'
+                        
+                        if (b+1) % 10 == 0:
+                            sleep(60)
+                else:
+                    pass
+        df_read.to_excel(self.dados['caminho'] + f"\\teste{b}.xlsx", index=False)
                 
     def result(self):
     # For de arquivo da pasta, sendo i o número do arquivo
-        for i, file in enumerate(os.listdir(parquet_id_dir)):
+        for i, file in enumerate(os.listdir(self.dados['parquet_id_dir'])):
 
             # Lendo arquivo por arquivo
-            df = pd.read_excel(parquet_id_dir + "\\" + file, engine="openpyxl")
+            df = pd.read_excel(self.dados['parquet_id_dir'] + "\\" + file, engine="openpyxl")
             id_list = df['RESULTADO']
             result = []
 
             # For de item por coluna, sendo E o número da linha
             for e, id in enumerate(id_list):
                 url = "url_api"
-                chamada = requests.get(url + id + 'paginacao_querystring', headers=headers)
+                chamada = requests.get(url + id + 'paginacao_querystring', headers=self.dados['headers'])
                 chamada = chamada.json()
 
                 # Validando se há dados para armazenamento
                 if 'data' in chamada:
                     a = pd.DataFrame(chamada['data'][0]['RESULTADO'])
-                    a.to_excel(caminho + f"\\teste{i, e}.xlsx", index=False)
+                    a.to_excel(self.dados['caminho'] + f"\\teste{i, e}.xlsx", index=False)
                     a = []
                 else:
-                    print(chamada)
-
-               
-            
+                    print(chamada)       
 
 if __name__ == '__main__':
-    service=consulta_cpf(True)
+    service=consulta_cpf()
     service.leitura()
