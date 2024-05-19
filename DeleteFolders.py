465fc7ce4b66c911b8ca23cbb09fdb32a3863b7c
natansales221 <natansales221@gmail.com>
2024-05-19 13:15:33 -0300

    inserindo orientação a objetos

--- DeleteFolders.py
+++ DeleteFolders.py
@@ -1,8 +1,19 @@
 import os
 
-dire = r'C:\Users\natan.sales\Desktop\teste\deletar'
-
-for file in os.listdir(dire):
-    caminho = os.path.join(dire, file)
-    if os.path.isfile(caminho):
-        os.remove(caminho)
+class delete():
+    
+    @property
+    def diretorio(self):
+        return{
+            'diretorio': r'C:\Users\natan\Desktop\teste\deletar'
+        }
+    def apaga_tudo(self):
+        for file in os.listdir(self.diretorio['diretorio']):
+            caminho = os.path.join(self.diretorio['diretorio'], file)
+            if os.path.isfile(caminho):
+                os.remove(caminho)
+                print(file, " apagado com sucesso")
+            
+if __name__ == '__main__':
+    service=delete()
+    service.apaga_tudo()
